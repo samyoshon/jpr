16fcff0c080bd64c812b3728cb034397f423c3ce
samyoshon <samuelyshon@gmail.com>
2015-12-17 14:45:40 -0800

    login/out navs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,14 +3,12 @@ class JobsController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
 
   def index
-  	@jobs = Job.where(["created_at > ?", 30.days.ago]).order("created_at DESC")
-  	@job = Job.new
+  	@jobs = Job.where(["created_at > ?", 30.days.ago]).order("created_at DESC").paginate(page: params[:page], per_page: 1)
+
   end
 
   def new
   	@job = current_user.jobs.build 
-
-  	@job = Job.new
   end
 
   def show
@@ -32,7 +30,7 @@ class JobsController < ApplicationController
   private 
 
   	def post_params
-  		params.require(:job).permit(:title, :description)
+  		params.require(:job).permit(:title, :description, :city_id, :country_id, :qualifications, :benefits_description, :benefits_airfare, :benefits_housing, :benefits_medical, :benefits_pto, :benefits_sick, :additional_info, :how_to)
   	end
 
 end
