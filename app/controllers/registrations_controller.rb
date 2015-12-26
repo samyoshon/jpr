5b87aed82e9ffb18e646014b651edbb50bebef70
samyoshon <samuelyshon@gmail.com>
2015-12-25 18:41:25 -0800

    update profile without new password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -20,8 +20,11 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
 
-  def registration_params
+	def update_resource(resource, params)
+    	resource.update_without_password(params)
+  	end
+  	def registration_params
   		params.require(:user).permit(:name, :description, :facebook, :website, :question_one, :question_two, :question_three, :question_four, :question_five, :answer_one, :answer_two, :answer_three, :answer_four, :answer_five, :logo)
-  end
+	end
 
 end
