5dff2ab83e2dff82d5960621383f5aec393985f0
samyoshon <samuelyshon@gmail.com>
2016-01-22 10:26:44 -0800

    s3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,4 +43,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+        :bucket => ENV['S3_BUCKET_NAME'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,9 +80,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['jetpath-assets'],
-      :access_key_id => ENV['AKIAIRPDCDM3XVDLS44Q'],
-      :secret_access_key => ENV['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']
+        :bucket => ENV['S3_BUCKET_NAME'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+  
 end
